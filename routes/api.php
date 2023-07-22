10cf757602588e1367380b1b1b1de4b0b15c7ae6
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-22 14:02:22 +0400

    feat: add sanctum setup

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/test', function () {
-    return response()->json(['foo' => 'bar']);
+
+Route::get('/test', function (Request $request) {
+    return response()->json([
+        'message' => 'Hello World!'
+    ]);
+});
+
+Route::middleware('auth:sanctum')->get('/login', function (Request $request) {
+    return response()->json([
+        'message' => 'Hello World!']);
 });
