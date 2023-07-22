9b3ab128178b761b5c52616920754353c8741bc9
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-22 15:48:53 +0400

    feat: update user migration

---
+++ database/migrations/2023_07_22_103025_alter_users_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('google_id')->nullable();
+            $table->string('image')->default(env('FRONTEND_URL') . '/assets/images/avatar.png');
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_blocked')->default(false);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['google_id', 'image', 'is_admin', 'is_active', 'is_blocked']);
+        });
+    }
+};
