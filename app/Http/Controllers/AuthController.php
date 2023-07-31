2351a8a441768f8deab4d52e1d39ae82579529da
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-07-31 20:05:42 +0400

    refactor: auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -108,7 +108,7 @@ class AuthController extends Controller
         auth()->login($user);
         return response()->json([
             'message' => 'user logged in',
-            'user' => auth()->user(),
+            'user' => $user
         ]);
     }
 
